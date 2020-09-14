7bc324f84d81ea1264c34228bc996f8077526c6a
Doctorwu <wuzhouchun@qq.com>
2020-09-14 13:54:15 +0800

    列表项可收起展开

---
+++ public/js/what_module.js
@@ -0,0 +1,16 @@
+(function() {
+    let whatList = document.querySelector('.what-list');
+    whatList.addEventListener('click', function(ev) {
+        console.log(ev);
+        let listItem = ev.path.find(p => p.className && p.className.includes("what-list-item"));
+        if (listItem) {
+            if (!listItem.className.includes("active")) {
+                listItem.classList.add("active");
+            } else {
+                if (ev.target.classList.contains("collapse")) {
+                    listItem.classList.remove("active");
+                }
+            }
+        }
+    })
+})()
