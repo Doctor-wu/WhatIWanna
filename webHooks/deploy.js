9758109941cee4ce9017a62b31fccb689984faf9
Doctorwu <wuzhouchun@qq.com>
2020-09-14 12:56:02 +0800

    server push

---
+++ webHooks/deploy.js
@@ -0,0 +1,42 @@
+const http = require('http')
+const createHandler = require('github-webhook-handler')
+const handler = createHandler({ path: '/', secret: 'root' })
+const cmd = require('node-cmd');
+// 上面的 secret 保持和 GitHub 后台设置的一致
+
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
+
+  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+  child.stdout.on('end', function() { callback (resp) });
+}
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  });
+  cmd.run(`./deploy.sh`);
+}).listen(7777,function(){
+  console.log("webhooks server is running at 7777");
+})
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+    switch (event.path) {
+    case '/':
+      run_cmd('sh', ['./deploy.sh', event.payload.repository.name], function (text) { console.log(text) })
+      break
+    default:
+      // 处理其他
+      break
+  }
+})
