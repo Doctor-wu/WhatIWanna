80cc69aea29be55cf40febb75fe5ba0354400ae4
Doctorwu <wuzhouchun@qq.com>
2020-09-15 10:04:36 +0800

    动画优化

--- public/js/drag.js
+++ public/js/drag.js
@@ -1,31 +1,57 @@
-export function Drag(el) {
+export function Drag(el, options = {}) {
     if (!this instanceof Drag) {
         return new Drag(el);
     }
     if (!el) throw new Error("Drag 构造函数需要传入指定元素");
     el = typeof el === "string" ? document.querySelector(el) : el;
-    this.init(el);
+    this.init(el, options);
     this.dragable();
 }
 
 
-Drag.prototype.init = function(el) {
+Drag.prototype.init = function(el, options) {
     el.style.position = 'absolute';
+
     this.el = el;
+
+    this.once = options.once;
+
+    this.snap = options.snap || true;
+
+    this.snapX = options.snapX || "0.25rem";
+
     this.currX = this.el.offsetLeft;
+
     this.currY = this.el.offsetTop;
+
     console.log(this.currX, this.currY);
+
     this.el.style.left = this.currX + 'px';
+
     this.el.style.top = this.currY + 'px';
+
     this.el.style.right = "unset";
+
     this.el.style.bottom = "unset";
+
     this.windowWidth = window.outerWidth;
 }
 
 Drag.prototype.dragable = function() {
-    this.el.addEventListener('touchstart', handleTouchStart.bind(this), false);
-    this.el.addEventListener('touchmove', handleTouchMove.bind(this), false);
-    this.el.addEventListener('touchend', handleTouchEnd.bind(this), false);
+    this.handleS = handleTouchStart.bind(this);
+    this.handleM = handleTouchMove.bind(this);
+    this.handleE = handleTouchEnd.bind(this);
+    this.el.addEventListener('touchstart', this.handleS, false);
+    this.el.addEventListener('touchmove', this.handleM, false);
+    this.el.addEventListener('touchend', this.handleE, false);
+}
+
+Drag.prototype.destroy = function() {
+    console.log(this);
+    this.el.removeEventListener('touchstart', this.handleS, false);
+    this.el.removeEventListener('touchmove', this.handleM, false);
+    this.el.removeEventListener('touchend', this.handleE, false);
+    this.el = null;
 }
 
 function handleTouchStart(ev) {
@@ -34,7 +60,6 @@ function handleTouchStart(ev) {
     let touch = ev.changedTouches[0];
     this.gapX = touch.clientX - this.currX;
     this.gapY = touch.clientY - this.currY;
-    return false;
 }
 
 function handleTouchMove(ev) {
@@ -54,13 +79,16 @@ function handleTouchEnd(ev) {
     ev.stopPropagation();
     ev.stopImmediatePropagation();
     let touch = ev.changedTouches[0];
-    if (touch.clientX > this.windowWidth / 2) {
-        this.el.style.right = ".25rem";
-        this.el.style.left = "unset";
-    } else {
-        this.el.style.left = ".25rem";
-        this.el.style.right = "unset";
+    if (this.snap) {
+        if (touch.clientX > this.windowWidth / 2) {
+            this.el.style.right = this.snapX;
+            this.el.style.left = "unset";
+        } else {
+            this.el.style.left = this.snapX;
+            this.el.style.right = "unset";
+        }
     }
     this.currX = this.el.offsetLeft;
     this.currY = this.el.offsetTop;
+    this.once && this.destroy();
 }
--- public/js/what_form.js
+++ public/js/what_form.js
@@ -3,12 +3,19 @@ import { Drag } from './drag.js';
 (function() {
     let formWrap = document.querySelector(".what-form-wrap");
     let close = document.querySelector('.close-what-form');
-    let popOut = document.querySelector(".add-schedule");
-    new Drag(popOut);
+    let whatList = document.querySelector('.what-list');
+    let popDrag = new Drag('.add-schedule', {
+        snapX: ".25rem"
+    });
+    let popOut = popDrag.el;
+    console.log(popDrag);
+
     popOut.addEventListener('click', () => {
         formWrap.classList.remove("hide");
+        whatList.classList.add("hide");
     })
     close.addEventListener('click', () => {
         formWrap.classList.add("hide");
+        whatList.classList.remove("hide");
     });
 })()
