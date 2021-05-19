4bf8d7f7f7165f6c46a74b253953eea34029dce2
doctorwu <doctorwu@tencent.com>
2021-05-19 09:54:27 +0800

    feat: 首页样式

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,8 @@ import DetailView2 from "../libs/view/template/route-test/detail2.js";
 import parseHTML from "../libs/view/compiler/parseHTML.js";
 import generate from "../libs/view/compiler/genCode.js";
 import createVnode from "../libs/view/compiler/create-vnode.js";
+import { VFormItem } from "../libs/vForm/vForm-item.js";
+import { VForm } from "../libs/vForm/vForm.js";
 
 export let whiteList = ["/auth/login", "/auth/regist", "/route-test", "/route-test/detail"];
 
@@ -111,30 +113,92 @@ View.usePlugin({
 
 View.usePlugin(createVnode);
 
+let user = new VFormItem({
+  tag: "input",
+  label: "用户名",
+  key: "casId",
+  attrs: {
+    placeholder: "请输入用户名[学号]",
+    autocomplete: "username"
+  },
+  rules: [
+    { prop: "required", msg: "请输入用户名[学号]", trigger: "blur" },
+    { pattern: /^\d{12}$/, msg: "请输入正用户名[学号]", trigger: "input" }
+  ]
+});
+
+let pwd = new VFormItem({
+  tag: "input",
+  label: "密码",
+  key: "password",
+  attrs: {
+    type: "password",
+    placeholder: "请输入密码",
+    autocomplete: "current-password"
+  },
+  rules: [
+    { prop: "required", msg: "请输入密码", trigger: "blur" }
+  ]
+});
+
+let vForm = new VForm({
+  title: "登录",
+  items: [user, pwd],
+  showBtn: false
+});
+let dom = vForm.mount("#login");
+
+
+let authTemplate = `<section class="auth">
+        <div class="login">
+        <h3>登录帐号</h3>
+        <div id="login" style="height: 4rem">${dom.outerHTML}</div>
+        <div class=".btn-wrap">
+        <button @click="handleLogin" class="btn btn-12 btn-success goLogin">登录</button>
+        <button class="btn btn-12 btn-default goRegist">注册</button>
+        </div>
+        </div>
+        <ul class="left">
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <ul class="right">
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </section>`
+
+
+
+
+
 let view = new View({
   name: "vm",
-  template: `<div id="auth">
-    <div class="login">
-    <h3 v-for="item in list" v-bind:style="loginStyle">登录 {{item.name}}帐号</h3>
-    <div id="login" style="height: 1rem;margin: 1rem 0;" v-if="needLogin">__login-form__</div>
-    <template v-for="(item, index) in numbers">
-      <div v-bind:class="item" v-if="item%2 !== 0">
-        ({{index}})奇数: {{item}}
-      </div>
-      <div v-else-if="Math.random() > 0.5">
-        ({{index}})偶数: {{item}}
-      </div>
-      <div v-else>
-        我是else
-      </div>
-    </template>
-    <div class="static">static</div>
-    <div class="btn-wrap">
-      <button @click="handleLogin" class="btn btn-12 btn-success goLogin">{{state}}</button>
-      <button class="btn btn-12 btn-default goRegist">注册</button>
-    </div>
-  </div>
-  </div>`,
+  template: authTemplate,
+  // `<div id="auth">
+  //   <div class="login">
+  //   <h3 v-for="item in list" v-bind:style="loginStyle">登录 {{item.name}}帐号</h3>
+  //   <div id="login" style="height: 1rem;margin: 1rem 0;" v-if="needLogin">__login-form__</div>
+  //   <template v-for="(item, index) in numbers">
+  //     <div v-bind:class="item" v-if="item%2 !== 0">
+  //       ({{index}})奇数: {{item}}
+  //     </div>
+  //     <div v-else-if="Math.random() > 0.5">
+  //       ({{index}})偶数: {{item}}
+  //     </div>
+  //     <div v-else>
+  //       我是else
+  //     </div>
+  //   </template>
+  //   <div class="static">static</div>
+  //   <div class="btn-wrap">
+  //     <button @click="handleLogin" class="btn btn-12 btn-success goLogin">{{state}}</button>
+  //     <button class="btn btn-12 btn-default goRegist">注册</button>
+  //   </div>
+  // </div>
+  // </div>`,
   data: {
     list: [{ name: 'doctorwu' }, { name: 'yoqi' }],
     numbers: [1, 2, 3, 4, 5],
